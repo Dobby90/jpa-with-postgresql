40045729f15dbd5f01282d6c649179e6fe067012
조홍연 <hycho@jasonsystem.co.kr>
2022-11-30 16:14:43 +0900

    init

---
+++ src/test/java/com/example/jpa_postgresql/JpaPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
